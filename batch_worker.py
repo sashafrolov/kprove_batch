3b19ff013d5dbbbcbc1875d6406bd775b9553141
Sasha F <sasha07974@gmail.com>
2021-03-28 10:27:28 -0400

    Added more info to batch worker

--- batch_worker.py
+++ batch_worker.py
@@ -2,7 +2,7 @@ import requests
 import json
 import sys
 sys.path.append('../mev/')
-import find_mev_kprove_uniswapv2
+from find_mev_kprove_uniswapv2 import reordering_mev
 
 
 ADDRESS = sys.argv[1]
@@ -11,6 +11,10 @@ if  __name__ == "__main__":
     commandData = requests.get(ADDRESS + "getData/2").text
     commandJson = json.loads(commandData)
     # do something with it at this point
+    temp = open("temp.txt", "w")
+    temp.write(command_json['spec_file'])
+    reordering_mev(commandJson['transactions'], "temp.txt", "out.txt", commandJson['acc'], commandJson['tokens'], commandJson['balances'], commandJson['pre_price'], commandJson['post_price'])
+
     resp = {"key": "value"}
     commandResponse = requests.post(ADDRESS, data = resp) 
 
