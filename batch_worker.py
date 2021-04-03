44a3300f65d0f2d18f1a2f4d076cbad21037179b
Sasha F <sasha07974@gmail.com>
2021-04-03 16:52:01 -0400

    Small fix to bound.k behavior

--- batch_worker.py
+++ batch_worker.py
@@ -10,9 +10,6 @@ ADDRESS = sys.argv[1]
 if  __name__ == "__main__":
     commandData = requests.get(ADDRESS + "getData/2").text
     commandJson = json.loads(commandData)
-    # do something with it at this point
-    temp = open("bound.k", "w")
-    temp.write(commandJson['spec_file'])
     # os.system("kompile mev.k --backend haskell")
 
     reordering_mev(commandJson['transactions'], "bound.k", "out.txt", commandJson['acc'], commandJson['tokens'], commandJson['balances'], commandJson['pre_price'], commandJson['post_price'])
